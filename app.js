1862af856f0da0d031d2593db8940a45d79e1d48
Aom Wara <waranat.cpe@gmail.com>
2022-11-18 17:27:49 +0700

    fix: my address

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 async function main() {
   require("dotenv").config();
-  const { API_URL, PRIVATE_KEY, TRANSFER_TO } = process.env;
+  const { API_URL, PRIVATE_KEY, TRANSFER_TO, MY_ADDRESS } = process.env;
   const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
   const web3 = createAlchemyWeb3(API_URL);
-  const myAddress = "0x1c0f0Af3262A7213E59Be7f1440282279D788335";
+  const myAddress = MY_ADDRESS;
 
   const nonce = await web3.eth.getTransactionCount(myAddress, "latest");
 
